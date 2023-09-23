f487505119387294577be8950c825bc7b07fd9c1
Baron Effeh <effehbaron@gmail.com>
2023-09-23 14:26:58 +0100

    First commit - all files at once

---
+++ README.md
@@ -0,0 +1,42 @@
+## Checkout form with interactive card details
+[Github Pages live](https://alexcumplido.github.io/frontend-projects/interactive-card-form/)
+
+This project is a form component with interactive card details.
+
+### Table of contents
+- [User flow](#user-flow)
+- [Demo](#demo)
+- [Run project](#run-project)
+- [Built with](#built-with)
+- [Thoughts](#thoughts)
+- [Continued development](#continued-development)
+
+#### User flow
+  - Update card details as the user fills in the fields
+  - Validate form fields once submitted
+  - Receive error messages when the form is submitted
+  - If form data is valid display modal continuation
+  - When user clicks continue reset the form
+  - View the optimal layout depending on device screen
+  - See hover, adctive and focus states for all interactive elements
+
+#### Demo
+![Project demo](./images/formComponent.gif)
+
+#### Run project
+```
+git clone git@github.com:alexcumplido/frontend-projects.git
+# Then a local development server like Live Server.
+```
+
+#### Built with
+- Semantic HTML5 markup
+- CSS custom properties
+- JavaScript
+- Custom Regular Expression
+- Mobile-first workflow
+
+#### Thoughts
+https://stackoverflow.com/questions/25101781/javascript-regex-split-credit-card-numbers
+
+#### Continued development
---
+++ scripts/script.js
@@ -0,0 +1,211 @@
+
+const form = document.querySelector('[name="form-card"]');
+
+const cardDetails = document.querySelectorAll('.card__detail');
+const formInputs = document.querySelectorAll('.form__input');
+
+const detailNumber = document.getElementById('detail-number');
+const detailCardholder = document.getElementById('detail-cardholder');
+const detailMonth = document.getElementById('detail-month');
+const detailYear = document.getElementById('detail-year');
+const detailCvc = document.getElementById('detail-cvc');
+
+const inputNumber = document.getElementById('card-number');
+const inputCardholder = document.getElementById('card-cardholder');
+const inputEmail = document.getElementById('card-email');
+const inputMonth = document.getElementById('card-month');
+const inputYear = document.getElementById('card-year');
+const inputCvc = document.getElementById('card-cvc');
+const errorNumber = document.getElementById('error-number');
+const errorCardholder = document.getElementById('error-cardholder');
+const errorEmail = document.getElementById('error-email');
+const errorMonth = document.getElementById('error-month');
+const errorYear = document.getElementById('error-year');
+const errorCvc = document.getElementById('error-cvc');
+
+const submitButton = document.getElementById('form-button');
+
+
+const modal = document.getElementById('modal');
+const modalContinuation = document.getElementById('modal-continuation');
+
+const regex = {
+    emptyInput: /^$/,
+    cardholder: /^[a-zA-Z]{2,}\s[a-zA-Z]{2,}\s[a-zA-Z]{2,}$/i,
+    number: /^[0-9]{16}$/,
+    email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i,
+    month: /(^[0][1-9]$)|(^[1][0-2]$)/,
+    year: /(^[2][2-9]$)/,
+    cvc: /^[0-9]{3}$/
+}
+
+const errorText = {
+    empty: `Can't be blank`,
+    carholderFormat: `Only letters, separate name and fullname`,
+    numberFormat: `Wrong format, only 16 numbers`,
+    emailFormat: `Invalid email`,
+    monthFormat: `Invalid month`,
+    yearFormat: `Invalid year`,
+    cvcFormat: `Invalid cvc`
+}
+
+function showError(errorEl, inputEl, text) {
+    errorEl.textContent = `${text}`;
+    errorEl.classList.remove('hidden');
+    inputEl.classList.add('form__input--error');
+}
+
+function hideError(errorEl, inputEl) {
+    errorEl.textContent = ``;
+    errorEl.classList.add('hidden');
+    inputEl.classList.remove('form__input--error');
+}
+
+function validateCardholder() {
+    let validation = false;
+    if (regex.emptyInput.test(inputCardholder.value.trim())) {
+        showError(errorCardholder, inputCardholder, errorText.empty);
+    } else if (regex.cardholder.test(inputCardholder.value.trim()) === false) {
+        showError(errorCardholder, inputCardholder, errorText.carholderFormat);
+    } else {
+        hideError(errorCardholder, inputCardholder);
+        validation = true;
+    }
+    return validation;
+}
+
+function validateNumber() {
+    let validation = false;
+    if (regex.emptyInput.test(inputNumber.value)) {
+        showError(errorNumber, inputNumber, errorText.empty);
+    } else if (regex.number.test(inputNumber.value) === false) {
+        showError(errorNumber, inputNumber, errorText.numberFormat);
+    } else {
+        hideError(errorNumber, inputNumber);
+        validation = true;
+    }
+    return validation;
+}
+
+function validateEmail() {
+    let validation = false;
+    if (regex.emptyInput.test(inputEmail.value)) {
+        showError(errorEmail, inputEmail, errorText.empty);
+    } else if (regex.email.test(inputEmail.value) === false) {
+        showError(errorEmail, inputEmail, errorText.emailFormat);
+    } else {
+        hideError(errorEmail, inputEmail);
+        validation = true;
+    }
+    return validation;
+}
+
+
+function validateMonth() {
+    let validation = false;
+    if (regex.emptyInput.test(inputMonth.value)) {
+        showError(errorMonth, inputMonth, errorText.empty);
+    } else if (regex.month.test(inputMonth.value) === false) {
+        showError(errorMonth, inputMonth, errorText.monthFormat);
+    } else {
+        hideError(errorMonth, inputMonth);
+        validation = true;
+    }
+    return validation;
+}
+
+function validateYear() {
+    let validation = false;
+    if (regex.emptyInput.test(inputYear.value)) {
+        showError(errorYear, inputYear, errorText.empty);
+    } else if (regex.year.test(inputYear.value) === false) {
+        showError(errorYear, inputYear, errorText.yearFormat);
+    } else {
+        hideError(errorYear, inputYear);
+        validation = true;
+    }
+    return validation;
+}
+
+function validateCvc() {
+    let validation = false;
+    if (regex.emptyInput.test(inputCvc.value)) {
+        showError(errorCvc, inputCvc, errorText.empty);
+    } else if (regex.cvc.test(inputCvc.value) === false) {
+        showError(errorCvc, inputCvc, errorText.cvcFormat);
+    } else {
+        hideError(errorCvc, inputCvc);
+        validation = true;
+    }
+    return validation;
+}
+
+function _forEach(list, callback) {
+    if (Array.isArray(list)) {
+        for (let index = 0; index < list.length; i++) {
+            callback(list[index], index, list);
+        }
+    } else {
+        for (let key in list) {
+            callback(list[key], key, list);
+        }
+    }
+}
+
+function cleanCardDetails() {
+    _forEach(cardDetails, function (element) {
+        element.textContent = ''
+    });
+}
+
+function cleanFormInputs() {
+    _forEach(formInputs, function (element) {
+        element.value = ''
+    });
+}
+
+function toggleModal() {
+
+    form.classList.toggle('hidden');
+    modal.classList.toggle('hidden');
+}
+
+inputCardholder.addEventListener('input', function (event) {
+    detailCardholder.textContent = event.target.value;
+});
+
+inputNumber.addEventListener('input', function (event) {
+    detailNumber.textContent = event.target.value.replace(/(\d{4}(?!\s))/g, "$1 ");
+});
+
+inputMonth.addEventListener('input', function (event) {
+    detailMonth.textContent = event.target.value;
+});
+
+inputYear.addEventListener('input', function (event) {
+    detailYear.textContent = event.target.value;
+});
+
+inputCvc.addEventListener('input', function (event) {
+    detailCvc.textContent = event.target.value;
+});
+
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    let cardHolderTest = validateCardholder();
+    let numberTest = validateNumber();
+    let emailTest = validateEmail();
+    let monthTest = validateMonth();
+    let yearTest = validateYear();
+    let cvcTest = validateCvc();
+
+    if (cardHolderTest && numberTest && emailTest && monthTest && yearTest && cvcTest) {
+        toggleModal();
+    }
+});
+
+modalContinuation.addEventListener('click', function (event) {
+    toggleModal();
+    cleanCardDetails();
+    cleanFormInputs();
+});
